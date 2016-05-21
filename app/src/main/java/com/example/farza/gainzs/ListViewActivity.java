4723842b0ae1ca848e70300653fdb4003e7680c9
Tim Henry <Tim Henry>
2016-05-21 17:22:12 -0400

    Near Final List

--- app/src/main/java/com/example/farza/gainzs/ListViewActivity.java
+++ app/src/main/java/com/example/farza/gainzs/ListViewActivity.java
@@ -3,27 +3,20 @@ package com.example.farza.gainzs;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
-
-import restraunt.RestrauntViewActivity;
 
 public class ListViewActivity extends AppCompatActivity {
 
     Restaurant restaurants[] = new Restaurant[]
             {
-                    new Restaurant(R.mipmap.ic_launcher, "Restaurant One"),
-                    new Restaurant(R.mipmap.ic_launcher, "Restaurant Two"),
-                    new Restaurant(R.mipmap.ic_launcher, "Restaurant Three"),
-                    new Restaurant(R.mipmap.ic_launcher, "Restaurant Four"),
-                    new Restaurant(R.mipmap.ic_launcher, "Restaurant Five"),
-                    new Restaurant(R.mipmap.ic_launcher, "Restaurant Six"),
-                    new Restaurant(R.mipmap.ic_launcher, "Restaurant Seven"),
-                    new Restaurant(R.mipmap.ic_launcher, "Restaurant Eight")
+                    new Restaurant(R.mipmap.mcd, "McDonald's"),
+                    new Restaurant(R.mipmap.mcd, "McDonald's"),
+                    new Restaurant(R.mipmap.mcd, "McDonald's"),
+                    new Restaurant(R.mipmap.mcd, "McDonald's"),
+                    new Restaurant(R.mipmap.mcd, "McDonald's"),
             };
 
     @Override
