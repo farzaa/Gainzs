8adb461b686f9213a2a29bdc940005b970eedbca
Farza <contactclothinghousenow@gmail.com>
2016-05-21 15:01:59 -0400

    Added clas

---
+++ app/src/main/java/com/example/farza/gainzs/Models/SingleMeal.java
@@ -0,0 +1,17 @@
+package com.example.farza.gainzs.Models;
+
+/**
+ * Created by Farza on 5/21/16.
+ */
+
+//Did not create getters/setters due to time constraint
+public class SingleMeal {
+
+    int calories;
+    int fat;
+    int protein;
+    int sugar;
+
+    public SingleMeal() {}
+
+}
