2eb71e2c96e2fa8c0f9a34dc3827450f29833987
Tim Henry <Tim Henry>
2016-05-21 10:42:05 -0400

    Adding ListView with custom ListAdapter

--- app/src/main/java/com/example/farza/gainzs/ListViewActivity.java
+++ app/src/main/java/com/example/farza/gainzs/ListViewActivity.java
@@ -2,12 +2,47 @@ package com.example.farza.gainzs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListViewActivity extends AppCompatActivity {
+
+    Restaurant restaurants[] = new Restaurant[]
+            {
+                    new Restaurant(R.mipmap.ic_launcher, "Cloudy"),
+                    new Restaurant(R.mipmap.ic_launcher, "Showers"),
+                    new Restaurant(R.mipmap.ic_launcher, "Snow"),
+                    new Restaurant(R.mipmap.ic_launcher, "Storm"),
+                    new Restaurant(R.mipmap.ic_launcher, "Sunny"),
+                    new Restaurant(R.mipmap.ic_launcher, "Cloudy"),
+                    new Restaurant(R.mipmap.ic_launcher, "Showers"),
+                    new Restaurant(R.mipmap.ic_launcher, "Snow"),
+                    new Restaurant(R.mipmap.ic_launcher, "Storm"),
+                    new Restaurant(R.mipmap.ic_launcher, "Sunny"),
+                    new Restaurant(R.mipmap.ic_launcher, "Cloudy"),
+                    new Restaurant(R.mipmap.ic_launcher, "Showers"),
+                    new Restaurant(R.mipmap.ic_launcher, "Snow"),
+                    new Restaurant(R.mipmap.ic_launcher, "Storm"),
+                    new Restaurant(R.mipmap.ic_launcher, "Sunny"),
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_view);
+        setContentView(R.layout.main);
+        //Log.d("Yoooo", "Did we get here");
+        customAdapter adapter = new customAdapter(
+
+                this,
+
+                R.layout.activity_list_view,
+
+                restaurants
+        );
+
+        ListView listy = (ListView)findViewById(R.id.restaurant_listView);
+
+        listy.setAdapter(adapter);
     }
 }
