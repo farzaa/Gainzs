8b81e483152fa702f9d5b2c58aab3a7cd84b3cbc
Alec Dilanchian <alec.dilanchian@gmail.com>
2016-05-21 04:48:21 -0400

    Adding user input UI

--- app/src/main/java/com/example/farza/gainzs/MainActivity.java
+++ app/src/main/java/com/example/farza/gainzs/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.farza.gainzs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button savePriceButton = (Button)findViewById(R.id.storePriceButton);
+        savePriceButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
