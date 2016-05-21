e17c61ee5ec49aa5b7a62fb9f1b89984e613d086
Alec Dilanchian <alec.dilanchian@gmail.com>
2016-05-21 12:21:10 -0400

    Adding check for blank input

--- app/src/main/java/com/example/farza/gainzs/MainActivity.java
+++ app/src/main/java/com/example/farza/gainzs/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.farza.gainzs;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.*;
 
@@ -16,6 +18,18 @@ public class MainActivity extends AppCompatActivity {
         Button savePriceButton = (Button)findViewById(R.id.storePriceButton);
         savePriceButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                TextView error = (TextView)findViewById(R.id.errorMessage);
+                EditText userInput = (EditText)findViewById(R.id.inputText);
+
+                //-- Check for input --//
+                String inputString = userInput.getText().toString();
+
+                if(inputString.matches("")) {
+                    error.setText("YOU FOOL! ENTER SOMETHING");
+                    return;
+                } else {
+                    error.setText("");
+                }
 
                 Intent listOfRestaurants = new Intent(MainActivity.this, ListViewActivity.class);
                 startActivity(listOfRestaurants);
