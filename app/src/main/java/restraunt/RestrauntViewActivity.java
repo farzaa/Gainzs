fc255553bc4fce5c270db29f135e9e67d69761b8
Farza <contactclothinghousenow@gmail.com>
2016-05-21 12:43:46 -0400

    Added internet capability

--- app/src/main/java/restraunt/RestrauntViewActivity.java
+++ app/src/main/java/restraunt/RestrauntViewActivity.java
@@ -1,6 +1,11 @@
 package restraunt;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -12,13 +17,71 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class RestrauntViewActivity extends AppCompatActivity {
 
+    TextView tvIsConnected;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.restaurant_list);
+
+        tvIsConnected = (TextView) findViewById(R.id.tvIsConnected);
+
+
+        if(isConnected())
+        {
+            tvIsConnected.setBackgroundColor(0xFF00CC00);
+            tvIsConnected.setText("You are connected");
+        }
+
+        else
+        {
+            tvIsConnected.setText("You are NOT conncted");
+        }
+
+    }
+
+    public static String GET(String url)
+    {
+        InputStream inputStream = null;
+        String result = "";
+        try {
+
+            // create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+
+            // make GET request to the given URL
+            HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
+
+            // receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+
+            // convert inputstream to string
+            if(inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else
+                result = "Did not work!";
+
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+
+        return result;
     }
 
     //This function needs to be able to go back to the main screen.
@@ -28,6 +91,76 @@ public class RestrauntViewActivity extends AppCompatActivity {
         Log.d("tag", "Go back to view");
     }
 
+    public boolean isConnected(){
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Activity.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected())
+            return true;
+        else
+            return false;
+    }
+
+    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String[] urls) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+//        // onPostExecute displays the results of the AsyncTask.
+//        @Override
+//        protected void onPostExecute(String result) {
+//            textView.setText(result);
+//        }
+
+    }
+
+    //Method to actually go and GET data via a URL.
+    private String downloadUrl(String myurl) throws IOException {
+        InputStream is = null;
+        int len = 10;
+
+        try
+        {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d("Network Tag", "The response is: " + response);
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            //String contentAsString = readIt(is, len);
+            //return contentAsString;
+
+            Log.d("Oh god", is.toString());
+
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+
+            if (is != null)
+            {
+                is.close();
+            }
+        }
+
+        catch(Exception e)
+        {
+            Log.e("Exception", e.toString());
+        }
+
+        return is.toString();
+    }
 
 
 
