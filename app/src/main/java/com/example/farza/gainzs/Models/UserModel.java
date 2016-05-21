3b287e94a26de952bc83e65fe10070da4a8622c3
Alec Dilanchian <alec.dilanchian@gmail.com>
2016-05-21 11:45:58 -0400

    Adding getInputValue

--- app/src/main/java/com/example/farza/gainzs/Models/UserModel.java
+++ app/src/main/java/com/example/farza/gainzs/Models/UserModel.java
@@ -4,6 +4,9 @@ package com.example.farza.gainzs.Models;
  * Created by alecdilanchian on 5/21/16.
  */
 public class UserModel {
+    private int userPriceInput;
 
-    
+    public void getPriceInput(int userInput) {
+        userPriceInput = userInput;
+    }
 }
